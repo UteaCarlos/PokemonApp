dce80fab30b5787cef81fccc5117669724757c2b
Carlos Huanca <202000203c@utea.edu.pe>
2022-12-01 02:49:43 -0500

    commit video 16 y video 17

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,12 +16,73 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Data"),
-          onPressed: () {
-            getDataPokemon();
-          },
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 12.0,
+                ),
+                const Text(
+                  "Pokedex",
+                  style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12.0,
+                  crossAxisSpacing: 12.0,
+                  childAspectRatio: 1.35,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 126, 227, 234),
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Stack(
+                        children: [
+                          Positioned(
+                            bottom: -22,
+                            right: -25,
+                            child: Image.asset(
+                              "assets/images/pokeball.png",
+                              height: 120.0,
+                              color: Colors.white.withOpacity(0.27),
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "Bulbasaur",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Image.network(
+                              "http://www.serebii.net/pokemongo/pokemon/001.png",
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
