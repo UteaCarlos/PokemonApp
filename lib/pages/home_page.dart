0ee1f297726b0dc29ac18b8ca88ea3ea1461c0a2
Carlos Huanca <202000203c@utea.edu.pe>
2022-11-30 20:30:43 -0500

    Commit_Video 16-2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,29 @@
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  // Uri
+  //Url
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        "https://raw.githubusercontent.com/Biuni/PokemonGo-pokedex/master/pokedex.json");
+
+    http.Response response = await http.get(_uri);
+    print(response.statusCode);
+    print(response.body);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Data"),
+          onPressed: () {
+            getDataPokemon();
+          },
+        ),
+      ),
+    );
   }
 }
