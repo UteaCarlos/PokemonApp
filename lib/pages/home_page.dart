cde7f94e5dee88a36a57db601818f4be9348e91b
Carlos Huanca <202000203c@utea.edu.pe>
2022-12-05 00:30:40 -0500

    commit video 19

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,44 @@
+import 'dart:convert';
+import 'dart:math';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:pokemon_app/models/pokemon_model.dart';
+import 'package:pokemon_app/ui/widgets/item_pokemon_widget.dart';
 
-class HomePage extends StatelessWidget {
-  // Uri
-  //Url
-  getDataPokemon() async {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List pokemons = [];
+  List<PokemonModel> pokemonsModel = [];
+
+  @override
+  initState() {
+    super.initState();
+    getdataPokemon();
+  }
+
+  getdataPokemon() async {
     Uri _uri = Uri.parse(
-        "https://raw.githubusercontent.com/Biuni/PokemonGo-pokedex/master/pokedex.json");
-
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
     http.Response response = await http.get(_uri);
-    print(response.statusCode);
-    print(response.body);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> myMap = json.decode(response.body);
+      //  pokemons = myMap["pokemon"];
+      pokemonsModel = myMap["pokemon"]
+          .map<PokemonModel>((e) => PokemonModel.fromJson(e))
+          .toList();
+      print(pokemonsModel);
+      setState(() {});
+
+//pokemons.forEach((element){
+      // print(element["name"]);
+//});
+    }
   }
 
   @override
@@ -26,7 +54,7 @@ class HomePage extends StatelessWidget {
                 const SizedBox(
                   height: 12.0,
                 ),
-                const Text(
+                Text(
                   "Pokedex",
                   style: TextStyle(fontSize: 34.0, fontWeight: FontWeight.bold),
                 ),
@@ -34,52 +62,22 @@ class HomePage extends StatelessWidget {
                   height: 30.0,
                 ),
                 GridView.count(
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12.0,
-                  crossAxisSpacing: 12.0,
-                  childAspectRatio: 1.35,
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 126, 227, 234),
-                        borderRadius: BorderRadius.circular(18),
-                      ),
-                      child: Stack(
-                        children: [
-                          Positioned(
-                            bottom: -22,
-                            right: -25,
-                            child: Image.asset(
-                              "assets/images/pokeball.png",
-                              height: 120.0,
-                              color: Colors.white.withOpacity(0.27),
-                            ),
+                    physics: const ScrollPhysics(),
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 12.0,
+                    crossAxisSpacing: 12.0,
+                    childAspectRatio: 1.35,
+                    children: pokemonsModel
+                        .map(
+                          (e) => ItemPokemonWidget(
+                            // name: e.name,
+                            //image: e.img,
+                            //types: e.type,
+                            pokemon: e,
                           ),
-                          Column(
-                            children: [
-                              Text(
-                                "Bulbasaur",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 16.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Positioned(
-                            bottom: 0,
-                            right: 0,
-                            child: Image.network(
-                              "http://www.serebii.net/pokemongo/pokemon/001.png",
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                )
+                        )
+                        .toList()),
               ],
             ),
           ),
